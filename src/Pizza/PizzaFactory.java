4e77457b5289dd3c9f67c7bfc00ab21dd0e03058
YuriVologda <skyrimpro2012@yandex.ru>
2022-05-18 20:37:58 +0300

    apple

--- src/Pizza/PizzaFactory.java
+++ src/Pizza/PizzaFactory.java
@@ -2,10 +2,6 @@ package Pizza;
 
 public class PizzaFactory {
     private Pizza pizza;
-    private Pizza pizza1;
-    private Pizza pizza2;
-    private Pizza pizza3;
-    private Pizza pizza4;
 
     public Pizza createMargarita() {
         pizza = new Pizza();
@@ -20,54 +16,54 @@ public class PizzaFactory {
     }
 
     public Pizza cratePepperoni() {
-        pizza1 = new Pizza();
-        pizza1.setName("Пепперони");
-        pizza1.setYeastDough(true);
-        pizza1.setMozzarella(true);
-        pizza1.setPepperoniSausage(true);
-        pizza1.setTomatoSauce(true);
-        pizza1.setDriedBasil(true);
-        return pizza1;
+        pizza = new Pizza();
+        pizza.setName("Пепперони");
+        pizza.setYeastDough(true);
+        pizza.setMozzarella(true);
+        pizza.setPepperoniSausage(true);
+        pizza.setTomatoSauce(true);
+        pizza.setDriedBasil(true);
+        return pizza;
     }
 
     public Pizza crateAlcopchene() {
-        pizza2 = new Pizza();
-        pizza2.setName("Аль-Копчене");
-        pizza2.setYeastDough(true);
-        pizza2.setMozzarella(true);
-        pizza2.setTomatoSauce(true);
-        pizza2.setSmokedSausages(true);
-        pizza2.setHam(true);
-        pizza2.setMushrooms(true);
-        pizza2.setBacon(true);
-        pizza2.setDriedBasil(true);
-        return pizza2;
+        pizza = new Pizza();
+        pizza.setName("Аль-Копчене");
+        pizza.setYeastDough(true);
+        pizza.setMozzarella(true);
+        pizza.setTomatoSauce(true);
+        pizza.setSmokedSausages(true);
+        pizza.setHam(true);
+        pizza.setMushrooms(true);
+        pizza.setBacon(true);
+        pizza.setDriedBasil(true);
+        return pizza;
     }
 
     public Pizza crateGavayskaya() {
-        pizza3 = new Pizza();
-        pizza3.setName("Гавайская");
-        pizza3.setYeastDough(true);
-        pizza3.setMozzarella(true);
-        pizza3.setHam(true);
-        pizza3.setTomatoSauce(true);
-        pizza3.setCannedPineapple(true);
-        pizza3.setDriedBasil(true);
-        return pizza3;
+        pizza = new Pizza();
+        pizza.setName("Гавайская");
+        pizza.setYeastDough(true);
+        pizza.setMozzarella(true);
+        pizza.setHam(true);
+        pizza.setTomatoSauce(true);
+        pizza.setCannedPineapple(true);
+        pizza.setDriedBasil(true);
+        return pizza;
 
 
     }
 
     public Pizza crateDonBacon(){
-        pizza4 = new Pizza();
-        pizza4.setName("Дон Бекон");
-        pizza4.setYeastDough(true);
-        pizza4.setMozzarella(true);
-        pizza4.setBacon(true);
-        pizza4.setTomatoSauce(true);
-        pizza4.setFreshTomatoes(true);
-        pizza4.setDriedBasil(true);
-        return pizza4;
+        pizza = new Pizza();
+        pizza.setName("Дон Бекон");
+        pizza.setYeastDough(true);
+        pizza.setMozzarella(true);
+        pizza.setBacon(true);
+        pizza.setTomatoSauce(true);
+        pizza.setFreshTomatoes(true);
+        pizza.setDriedBasil(true);
+        return pizza;
     }
 
 }
